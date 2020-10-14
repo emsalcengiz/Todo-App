911f5f6cff0450f7a999e579d42e319458e4e67d
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-14 16:55:33 +0300

    updated

---
+++ examples.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    numbers = [1,4,7,8,9,6,3]
+    message = "mesaj gidecek mi !!"
+    return render_template("index_ex.html", number = 2020,number_two = 2030, message = message, numbers = numbers)
+
+
+if __name__ == '__main__':
+    app.run(debug = True, port=3000)
