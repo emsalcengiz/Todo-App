67920f9db272621fbdbecff2d242e6742a77844e
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-14 16:34:12 +0300

    updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", number = 2020,number_two = 2030)
 
 
 if __name__ == '__main__':
