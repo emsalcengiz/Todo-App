730c67b1b7d39f8541af68fbc21ac113846de647
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-14 16:45:52 +0300

    updated

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    numbers = [1,4,7,8,9,6,3]
     message = "mesaj gidecek mi !!"
-    return render_template("index.html", number = 2020,number_two = 2030, message = message)
+    return render_template("index.html", number = 2020,number_two = 2030, message = message, numbers = numbers)
 
 
 if __name__ == '__main__':
