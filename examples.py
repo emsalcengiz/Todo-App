cd62973437bd0d1f81dd1fcaabb363b575223d5e
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-14 18:02:18 +0300

    updated

--- examples.py
+++ examples.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +6,17 @@ def index():
     numbers = [1,4,7,8,9,6,3]
     message = "mesaj gidecek mi !!"
     return render_template("index_ex.html", number = 2020,number_two = 2030, message = message, numbers = numbers)
+
+@app.route("/sum", methods = ["GET","POST"])
+def sum():
+    if request.method == "POST":
+        number1 = request.form.get("number1")
+        number2 = request.form.get("number2")
+        return render_template("sum.html", total = int(number2) + int(number2))
+        
+    else:
+        return render_template("sum.html")
+
 
 
 if __name__ == '__main__':
