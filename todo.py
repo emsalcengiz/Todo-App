e7693eac2fcdb1d5bcd2dcc0f7f3d0e07cacc048
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-22 17:23:14 +0300

    updated

--- todo.py
+++ todo.py
@@ -3,6 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/emsal.cengiz/Desktop/Todo-App/Todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    content = db.Column(db.Text)
+    comlete = db.Column(db.Boolean)
+
+   
+if __name__ == "__main__":
+    app.run(debug=True)
