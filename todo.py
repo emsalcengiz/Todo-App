ae5cd1f489c1b9f82d9670e391e6d474d9558ed6
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-22 18:15:01 +0300

    updated

--- todo.py
+++ todo.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/emsal.cengiz/Desktop/Todo-App/Todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 class Todo(db.Model):
@@ -15,4 +18,4 @@ class Todo(db.Model):
 
    
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
