e0a2d265460af16d240049a66d935c379c43844f
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-23 17:16:54 +0300

    updated

--- todo.py
+++ todo.py
@@ -3,26 +3,43 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/emsal.cengiz/Desktop/Todo-App/Todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 @app.route('/')
 def index():
-    return render_template("index.html")
+    todos = Todo.query.all()
+    return render_template("index.html", todos = todos)
 
-@app.route("/add", methods=["POST"])
+@app.route("/add", methods=["GET", "POST"])
 def addTodo():
     title = request.form.get("title")
     content = request.form.get("content")
-    newTodo = Todo( title=title, content=content, complete= False)
+
+    newTodo = Todo(title=title, content=content, comlete= False)
+
     db.session.add(newTodo)
     db.session.commit()
-    return redirect(url_for("index.html"))
+    return redirect(url_for("index"))
+
+@app.route("/comlete/<int:id>", methods=["GET"])
+def comlete_Todo(id):
+    todo = Todo.query.filter_by(id=id).first()
+    if (todo.comlete == False):
+        todo.comlete = True
+    else:
+        todo.comlete = False
+    db.session.commit()
+    return redirect(url_for("index"))
+    
+    
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     content = db.Column(db.Text)
-    complete = db.Column(db.Boolean)
+    comlete = db.Column(db.Boolean)
+
 
    
 if __name__ == "__main__":
