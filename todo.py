7daab7424fcb69f1ef64db2435b9d39aabfb3f43
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-23 14:20:40 +0300

    updated

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,12 +9,20 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
+@app.route("/add", methods=["POST"])
+def addTodo():
+    title = request.form.get("title")
+    content = request.form.get("content")
+    newTodo = Todo( title=title, content=content, complete= False)
+    db.session.add(newTodo)
+    db.session.commit()
+    return redirect(url_for("index.html"))
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     content = db.Column(db.Text)
-    comlete = db.Column(db.Boolean)
+    complete = db.Column(db.Boolean)
 
    
 if __name__ == "__main__":
